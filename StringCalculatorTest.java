9c97fb4c096310bf1d17b1257926d8172b7f498f
benbjamin123 <ben.tgms@gmail.com>
2018-01-29 18:30:54 -0500

    Test 3
    Test for part 3 (String with 2 numbers)

--- StringCalculatorTest.java
+++ StringCalculatorTest.java
@@ -21,6 +21,18 @@ class StringCalculatorTest {
             result = functions.add(oneItem);
             assertEquals(x, result, "String with one item does not work");
         }
+
+        //string with two items
+        String twoItems;
+        int sum;
+        for(int x = -1; x < 2; x++){
+            for(int y = -1; y < 2; y++){
+                sum = x+y;
+                twoItems = Integer.toString(x) + "," + Integer.toString(y);
+                result = functions.add(twoItems);
+                assertEquals(sum, result, "String with two items does not work");
+            }
+        }
     }
 
 }
